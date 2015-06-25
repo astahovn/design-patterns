0d13d67b210f5606210fc33965382961d40601b5
evolver <evolverhome@yandex.ru>
2015-06-25 20:24:15 +0500

    константы событий закупки

--- observer/events/Procedure.php
+++ observer/events/Procedure.php
@@ -6,6 +6,15 @@
  */
 class Procedure implements Observable {
 
+  const EVENT_PUBLICATION               = 1;  // публикация закупки
+  const EVENT_REGISTRATION_OVER         = 2;  // окончание приема заявок
+  const EVENT_APPLIC_OPEN_START         = 3;  // начало вскрытия конвертов
+  const EVENT_PROTOCOL_APPLIC_OPENED    = 4;  // публикация протокола вскрытия конвертов
+  const EVENT_PROTOCOL_APPLIC_REVIEW    = 5;  // публикация протокола рассмотрения заявок
+  const EVENT_PROTOCOL_SUMMING_UP       = 6;  // публикация протокола подведения итогов
+  const EVENT_CONTRACT_START            = 7;  // начало этапа заключения договора
+  const EVENT_ARCHIVE                   = 8;  // переход в архив
+
   protected $events = array();
 
   /**
