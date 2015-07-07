3332c2885f2852574840e04e3fc5efb2bc9903d7
evolver <evolverhome@yandex.ru>
2015-07-08 00:40:02 +0500

    рассыльщик уведомлений

---
+++ observer/events/Notifier/Procedure.php
@@ -0,0 +1,21 @@
+<?php
+
+Namespace Notifier;
+
+use Observer\iObserver;
+use Procedure\Procedure as Proc;
+
+class Procedure implements iObserver {
+
+  /**
+   * Обработчик события
+   * @param string $event
+   * @param array $data
+   */
+  public function eventsListener($event, $data)
+  {
+    $eventText = Proc::getEventText($event);
+    var_dump("Procedure notifier: {$data['name']} -> {$eventText}");
+  }
+
+}
--- observer/events/index.php
+++ observer/events/index.php
@@ -7,14 +7,21 @@ function __autoload($class) {
 
 use Procedure\Procedure;
 use Logger\Procedure as LoggerProcedure;
+use Notifier\Procedure as NotifierProcedure;
 
 $procedure = new Procedure();
+
 $procedure->attach(new LoggerProcedure(), [
   Procedure::EVENT_PUBLICATION,
   Procedure::EVENT_REGISTRATION_OVER,
   Procedure::EVENT_PROTOCOL_PUBLISHED,
   Procedure::EVENT_ARCHIVE
 ]);
+
+$procedure->attach(new NotifierProcedure(), [
+  Procedure::EVENT_PROTOCOL_PUBLISHED
+]);
+
 $procedure->saveDraft('Test 1');
 $procedure->publish();
 $procedure->registrationOver();
