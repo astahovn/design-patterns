a57625ff7488fc1dc50b81788596651bfbc53bcc
evolver <evolverhome@yandex.ru>
2015-07-09 00:22:57 +0500

    тест для Наблюдателя

--- observer/events/Tests/ObserverTest.php
+++ observer/events/Tests/ObserverTest.php
@@ -9,12 +9,35 @@ use Procedure\Procedure;
 class ObserverTest extends \PHPUnit_Framework_TestCase
 {
 
-  public function testPublish()
+  /**
+   * Тестируем прослушивание заданных событий Наблюдателем
+   */
+  public function testEventsListener()
   {
+    // Создаем наблюдателя
+    $observer = $this->getMockBuilder('Observer\iObserver')
+      ->setMethods(array('eventsListener'))
+      ->getMock();
+
+    // Ожидаем, что наблюдатель получит уведомления по двум событиям
+    $observer->expects($this->exactly(2))
+      ->method('eventsListener')
+      ->withConsecutive(
+        $this->equalTo(Procedure::EVENT_PUBLICATION),
+        $this->equalTo(Procedure::EVENT_ARCHIVE)
+      );
+
     $procedure = new Procedure();
+    // Слушаем только 2 тестируемых события
+    $procedure->attach($observer, [
+      Procedure::EVENT_PUBLICATION,
+      Procedure::EVENT_ARCHIVE
+    ]);
     $procedure->saveDraft('Test');
     $procedure->publish();
-    $this->expectOutputString("Test - published\n");
+    $procedure->registrationOver();
+    $procedure->publishProtocol();
+    $procedure->archive();
   }
 
 }
