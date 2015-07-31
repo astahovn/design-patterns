ef7f3d6dcc47eb1233cd5d0624045009c4b69d15
evolver <evolverhome@yandex.ru>
2015-07-31 23:51:25 +0500

    procedure class implementation

--- Behavioral/State/Procedure/Procedure.php
+++ Behavioral/State/Procedure/Procedure.php
@@ -2,15 +2,58 @@
 
 Namespace DesignPatterns\Behavioral\State\Procedure;
 
+use DesignPatterns\Behavioral\State\AppList\AppListInterface;
+use DesignPatterns\Behavioral\State\AppList\AppListRegistration;
+use DesignPatterns\Behavioral\State\AppList\AppListProtocol;
+use DesignPatterns\Behavioral\State\AppList\AppListArchive;
+
+/**
+ * Class Procedure
+ * @package DesignPatterns\Behavioral\State\Procedure
+ */
 class Procedure
 {
+  const STATUS_REGISTRATION = 1;
+  const STATUS_PROTOCOL = 2;
+  const STATUS_ARCHIVE = 3;
+
+  /** @var int $status */
+  protected $status;
+
+  /**
+   * Set procedure status
+   * @param int $status
+   */
+  public function setStatus($status)
+  {
+    $this->status = $status;
+  }
 
   /**
    * Get application list object depends on procedure's status
+   * @return AppListInterface
+   * @throws \Exception
    */
   public function getAppList()
   {
+    switch($this->status) {
+      case self::STATUS_REGISTRATION:
+        $appList = new AppListRegistration($this);
+        break;
 
+      case self::STATUS_PROTOCOL:
+        $appList = new AppListProtocol($this);
+        break;
+
+      case self::STATUS_ARCHIVE:
+        $appList = new AppListArchive($this);
+        break;
+
+      default:
+        throw new \Exception('Unknown procedure status');
+    }
+
+    return $appList;
   }
 
 }
