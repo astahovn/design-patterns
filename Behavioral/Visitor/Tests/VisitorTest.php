9c897cd66d6fc6706110d7739974ab1c5c614653
evolver <evolverhome@yandex.ru>
2015-08-06 23:33:30 +0500

    some more asserts

--- Behavioral/Visitor/Tests/VisitorTest.php
+++ Behavioral/Visitor/Tests/VisitorTest.php
@@ -13,18 +13,22 @@ class VisitorTest extends \PHPUnit_Framework_TestCase
   {
     $procedure = new Procedure();
     $procedure->addUnit(
-      (new Product(['name' => 'PC', 'price' => 500, 'quantity' => 1, 'mark' => 'Mark II']))
+      new Product(['name' => 'PC', 'price' => 500, 'quantity' => 1, 'mark' => 'Mark II'])
     );
     $procedure->addUnit(
-      (new Service(['name' => 'OS install', 'price' => 5, 'quantity' => 1, 'specialistLevel' => 5]))
+      new Service(['name' => 'Install', 'price' => 5, 'quantity' => 1, 'specialistLevel' => 5])
     );
     $procedure->addUnit(
-      (new Product(['name' => 'OS', 'price' => 10, 'quantity' => 1, 'mark' => 'Some OS']))
+      new Product(['name' => 'OS', 'price' => 10, 'quantity' => 1, 'mark' => 'Some OS'])
     );
     list($products, $services) = $procedure->getUnitsInfo();
 
     $this->assertCount(2, $products, 'Wrong products count');
+    $this->assertRegExp('/(PC|OS)/', $products[0]);
+    $this->assertRegExp('/(PC|OS)/', $products[1]);
+
     $this->assertCount(1, $services, 'Wrong services count');
+    $this->assertRegExp('/(Install)/', $services[0]);
   }
 
 }
